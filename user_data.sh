d2e3c5a98b5e14287a7c6cba8200638712efeeb1
Юрий Михейкин <umiheikin@yandex.ru>
2019-11-07 11:54:18 +0300

    deleted ssh connection via pass from userdata an modify docker installation script

--- user_data.sh
+++ user_data.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-
-
-echo "Starting the script execution"
-#Create ssh user
-sudo echo -e "$remote_pass\nremote_pass" | passwd ec2-user
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo echo "AllowUsers ec2-user" >> /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
 #Install Docker
 
 echo "Starting to install docker"
@@ -16,19 +7,22 @@ echo "Starting to install docker"
 sudo yum install -y update
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce
+sudo yum update -y
+sudo yum install -y --nobest docker-ce
 sudo usermod -aG docker $(whoami)
 sudo usermod -aG docker ec2-user
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
+echo "Docker has been installed with version $(docker version)"
+
 #Install Docker Compose
 
 echo "Starting to install Docker Compose"
 
-sudo yum install -y epel-release
-sudo yum install -y python-pip
-sudo pip install -y docker-compose
-sudo yum upgrade python*
-docker-compose version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "The docker compose installed with version $(docker-compose version)"
+echo "Script has been finished"
 
