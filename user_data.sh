670ffedd0dd0aa49a9b04e9d2135b7d1970815b9
Юрий Михейкин <umiheikin@yandex.ru>
2019-11-08 18:50:32 +0300

    Changes zabbix-compose

--- user_data.sh
+++ user_data.sh
@@ -7,10 +7,10 @@ echo "Starting to install docker"
 sudo yum install -y update
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum update -y
 sudo yum install -y --nobest docker-ce
 sudo usermod -aG docker $(whoami)
 sudo usermod -aG docker ec2-user
+sg docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
@@ -25,4 +25,4 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 echo "The docker compose installed with version $(docker-compose version)"
 echo "Script has been finished"
-
+sg docker
